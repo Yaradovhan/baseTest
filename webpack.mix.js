0f880d8b7a5e66bc375c7af142e36a3bb8cb7af9
Yaroslav <yara.dovhan@gmail.com>
2019-08-28 20:41:51 +0300

    add register/auth user

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,15 +1,8 @@
 const mix = require('laravel-mix');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
-
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix
+    .setPublicPath('public/build')
+    .setResourceRoot('/build/')
+    .sass('resources/sass/app.scss', 'css')
+    .js('resources/js/app.js', 'js')
+    .version();
